b93a694262afaec63e67d33c50f6df57258653c7
VeenaArv <vla240@nyu.edu>
2019-12-18 12:08:58 -0500

    used regex to detect implicit vs explict

--- src/extract-features.py
+++ src/extract-features.py
@@ -1,4 +1,5 @@
 import os.path
+import re
 
 CURRENT_DIR = os.path.dirname(__file__)
 WIKI_DIR = os.path.join(CURRENT_DIR, '../data/curated_data/cleaned/')
@@ -13,17 +14,8 @@ def get_pdtb_filepath(file_name):
 def get_n_discourse_relations_from_pdtb_annotations(pdtb_filepath):
     with open(pdtb_filepath, 'r') as f:
         annotations = f.read()
-        n_implicit = 0
-        n_explicit = 0
-        for relation in annotations.split("|||||||||||||"):
-            relation_type = relation.split("|||")[0].strip()
-            if relation_type == "Implicit":
-                n_implicit += 1
-            elif relation_type == "Explicit":
-                n_explicit += 1
-            else:
-                # add value to logs
-                print(relation_type)
+        n_implicit = len(re.findall('Implicit\|', annotations))
+        n_explicit = len(re.findall('Explicit\|', annotations))
         return n_explicit, n_implicit
 
 
@@ -38,13 +30,15 @@ def write_feature_file(is_simple=False):
     wc_list = read_wc(is_simple)
     for doc_count in range(50):
         cleaned_file_name = 'simple' + str(doc_count) + '.txt' if is_simple else 'regular' + str(doc_count) + '.txt'
-        n_relations = get_n_discourse_relations_from_pdtb_annotations(get_pdtb_filepath(cleaned_file_name))
-        output_path = FEATURE_DIR + cleaned_file_name
-        with open(output_path, 'w+') as f:
-            norm_n_explicit = n_relations[0] / int(wc_list[doc_count])
-            norm_n_implicit = n_relations[1] / int(wc_list[doc_count])
-            f.write('n_explicit=' + str(norm_n_explicit) + '\n')
-            f.write('n_implicit=' + str(norm_n_implicit) + '\n')
+        print(cleaned_file_name)
+        print(get_pdtb_filepath(cleaned_file_name))
+        # n_relations = get_n_discourse_relations_from_pdtb_annotations(get_pdtb_filepath(cleaned_file_name))
+        # output_path = FEATURE_DIR + cleaned_file_name
+        # with open(output_path, 'w+') as f:
+        #     norm_n_explicit = n_relations[0] / int(wc_list[doc_count])
+        #     norm_n_implicit = n_relations[1] / int(wc_list[doc_count])
+        #     f.write('n_explicit=' + str(norm_n_explicit) + '\n')
+        #     f.write('n_implicit=' + str(norm_n_implicit) + '\n')
 
 
 def main():
